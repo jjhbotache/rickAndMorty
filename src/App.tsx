6fc3dc88e8559b351642fb505a7f5ec5949482b6
jjhbotache <jjhuertasbotache@gmail.com>
2023-06-13 09:33:10 -0500

    searching

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import './App.css';
 // import characterCard component
 
 import CharacterCard from './components/CharacterCard';
-import Filter from './components/Filter';
-import { Character } from './models/interfaces';
+import Filter from './components/Filter/Filter';
+import { type Character } from './models/interfaces';
 
 const apiRoute = `https://rickandmortyapi.com/api/character`;
 // create an url obj
@@ -24,19 +24,15 @@ function App() {
 
   const [characters,setCharacters] = useState<Character[]>([])
   const [load,setLoad] = useState<number>(1)
+  const [searched,setSearched] = useState<string>("")
+  const [nextUrlSearched,setNextUrlSearched] = useState<string>()
+  // const [filtered,setFiltered] = useState<string[]>()
 
-
-
-
-  Notification.requestPermission();
 
   useEffect(
     ()=>{
       
       document.addEventListener('scroll', ()=>{
-        // console.log("wih:", window.innerHeight);
-        // console.log("wsy:", window.scrollY);
-        // console.log("dbo:", document.body.offsetHeight);
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight-100) {
           setLoad((load) => load+1)
           console.log(load);
@@ -52,49 +48,115 @@ function App() {
     []
   )
 
-
-
   useEffect(()=>{
     // add a parameter to the url
-    const url = new URL(apiRoute);
-    url.searchParams.set("page",load.toString())
-    console.log(url);
+    if (searched==="") {
+      const url = new URL(apiRoute);
+      url.searchParams.set("page",load.toString())
+      console.log(url);
+      
+      fetch(url).then((response)=>response.json()).then((value) => {
+        console.log(value);
+        const finalList: Character[]= [...characters,...value.results]
+        setCharacters(finalList)
+      })
+    }else{
+      if (nextUrlSearched) {
+        const url = new URL(nextUrlSearched);
+        fetch(url).then((response)=>response.json()).then((value) => {
+          console.log(value);
+          const finalList: Character[]= [...characters,...value.results]
+          setCharacters(finalList)
+          setNextUrlSearched(value.info.next)
+        })
+      }
+    }
     
-    fetch(url).then((response)=>response.json()).then((value) => {
-      console.log(value);
-      const finalList: Character[]= [...characters,...value.results]
-      setCharacters(finalList)
-    })
-    new Notification("Number of characters", {
-      body: "You are in page "+load
-    });
   },[load])
 
-  
+
+//   name: filter by the given name.
+// status: filter by the given status (alive, dead or unknown).
+// species: filter by the given species.
+// type: filter by the given type.
+// gender: filter by the given gender (female, male, genderless or unknown).
+
 
     
+  useEffect(()=>{
+    if(searched){
+      const url = new URL(apiRoute);
+      url.searchParams.set("name",searched)
+      console.log(url);
 
-  
+      fetch(url).then((response)=>response.json()).then((value) => {
+        console.log(value);
+        const finalList: Character[]= [...value.results]
+        setCharacters(finalList)
+        setNextUrlSearched(value.info.next)
+      })
+    }
+    return(
+      setLoad(1),
+      setCharacters([])
+    )
+  },[searched])
 
-  const featuresToFilter = [
-    { name: "nombre", function: 1 },
-    { name: "estado", function: 1 }
-  ];
 
-  // set load to a random number
-
-  
 
   return (
     <>
 
+    {/* search bar */}
     <div className="input-group rounded">
-      <input type="text" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+      <input type="text" className="form-control" placeholder="Search" onChange={(e)=>{setSearched(e.target.value)}} />
       <button type="button" className="btn btn-primary" onClick={()=>{setLoad(Math.floor(Math.random() * 42) + 1)}} ><i className="fi fi-br-search"></i></button>
     </div>
 
-    <Filter featuresToFilter={featuresToFilter}></Filter>
+    {/* filter */}
+    <div className="btn-group">
+      <button className="btn btn-secondary dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+            Filter
+      </button>
+      <div className="dropdown-menu dropdown-menu-start" aria-labelledby="triggerId">
+      <div className="col">
+          <select id="statusFilter" className="form-control">
+            <option value="">All Statuses</option>
+            <option value="alive">Alive</option>
+            <option value="dead">Dead</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+        <div className="col">
+          <select id="speciesFilter" className="form-control">
+            <option value="">All Species</option>
+            <option value="Human">Human</option>
+            <option value="Alien">Alien</option>
+            
+          </select>
+        </div>
+        <div className="col">
+          <select id="typeFilter" className="form-control">
+            <option value="">All Types</option>
+            <option value="Type 1">Type 1</option>
+            <option value="Type 2">Type 2</option>
+            
+          </select>
+        </div>
+        <div className="col">
+          <select id="genderFilter" className="form-control">
+            <option value="">All Genders</option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+          </select>
+        </div>
+      </div>
+    </div>
     
+    {/* characters */}
     <div className="container-fluid">
       <div className="row g-3">
         {
