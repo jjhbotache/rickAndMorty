437ad474e24ee9b63728f2b13d8b0235d616ff90
jjhbotache <jjhuertasbotache@gmail.com>
2023-06-23 10:57:11 -0500

    2nd week fixing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import { useEffect, useState } from 'react';
-import './App.css';
 // import characterCard component
 
 import CharacterCard from './components/CharacterCard';
@@ -10,29 +9,42 @@ import { type APIResponse, type Character } from './models/interfaces';
 import { type Filter } from './models/interfaces';
 
 const apiRoute = `https://rickandmortyapi.com/api/character`;
-// create an url obj
 
-
-/*
-  organizable por nombre,especie
-  buscar nombre especie especifico
-  buscar por vivo? 
-  sin anys
-   */
 
 function App() {
 
   const [response,setResponse] = useState<APIResponse>()
   const [nextUrl,setNextUrl] = useState<string | null>(null)
-
   const [characters,setCharacters] = useState<Character[]>([])
+  const [searching,setSearching] = useState(false)
 
-  const [filter,setFilter] = useState<Filter>()
+
+  let filter:Filter;
+  function filterCharacter() {
+    let url = new URL(apiRoute)
+        // to do: optimize
+    if(filter.name){
+      url.searchParams.append("name",filter.name)
+    }
+    if(filter.status){
+      url.searchParams.append("status",filter.status)
+    }
+    if(filter.species){
+      url.searchParams.append("species",filter.species)
+    }
+    if(filter.gender){
+      url.searchParams.append("gender",filter.gender)
+    }
+    setSearching(true)
+    fetch(url).then(re=>re.json()).then((value) => {
+      setCharacters([])
+      setResponse(value)
+    })
+  }
 
   useEffect(
-    // when it gets the final of the page,
-    // it will load the next page
     ()=>{
+      setSearching(true)
       fetch(apiRoute).then((re)=>re.json()).then((value) => {
         setResponse(value)
       })      
@@ -42,11 +54,14 @@ function App() {
   
   useEffect(
     ()=>{
+      setSearching(false)
       if (response){
         console.log(response);
-        if (response.results) {
-          setCharacters([...characters,...response.results])
-        }
+        setCharacters(
+          Array.isArray(response.results)
+          ?[...characters,...response.results]
+          :[]
+        )
         window.addEventListener('scroll',()=>{
           if(window.scrollY + window.innerHeight >= document.body.scrollHeight -100){
             console.log("end of page");
@@ -54,6 +69,9 @@ function App() {
             setNextUrl(response.info.next)
           }
         })
+      }else{
+        console.log("no response");
+        setCharacters([])
       }
       return ()=>{
         window.removeEventListener('scroll',()=>{})
@@ -66,6 +84,7 @@ function App() {
   useEffect(
    ()=>{
     if(nextUrl){
+      setSearching(true)
       fetch(nextUrl).then(re=>re.json()).then((value) => {
         setResponse(value)
       })
@@ -73,29 +92,7 @@ function App() {
    } 
   ,[nextUrl])
 
-  useEffect(
-    ()=>{
-      if(filter){
-        let url = new URL(apiRoute)
-        if(filter.name){
-          url.searchParams.append("name",filter.name)
-        }
-        if(filter.status){
-          url.searchParams.append("status",filter.status)
-        }
-        if(filter.species){
-          url.searchParams.append("species",filter.species)
-        }
-        if(filter.gender){
-          url.searchParams.append("gender",filter.gender)
-        }
-        fetch(url).then(re=>re.json()).then((value) => {
-        setCharacters([])
-        setResponse(value)
-        })
-      }
-    },
-  [filter])
+
 
   
 
@@ -108,74 +105,112 @@ function App() {
 // gender: filter by the given gender (female, male, genderless or unknown).
 
 
-
+// bug display in just one (alive human unknow) .✔️
+// bug display bar no static✔️
+// missing spinner
+// missing space between cards✔️
+// better design for the filter✔️
+// del useEffect and its ifs
 
 
   return (
     <>
 
     {/* search bar */}
-    <div className="input-group rounded">
-      <input type="text" className="form-control" placeholder="Search" onChange={(e)=>{setFilter({...filter,name:e.target.value})}} />
-    </div>
+    <div className="container-fluid" style={
+
+      {
+        background:"#222"
+      }
+    }>
+      <div className="row rounded"  style={
+        {
+          position:"sticky",
+          top:0,
+          paddingTop:"1em",
+          zIndex:1000,
+          backgroundColor:"#222"
+        }
+      }>
+        <input type="text" className="form-control " placeholder="Search" onChange={(e)=>{
+          filter = {...filter,name:e.target.value};
+          filterCharacter()
+          }} />
 
     {/* filter */}
-    <div className="btn-group">
-      <button className="btn btn-secondary dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
-          aria-expanded="false">
-            <i className="fi fi-rr-settings-sliders"></i>
-      </button>
-      <div className="dropdown-menu dropdown-menu-start" aria-labelledby="triggerId">
-      <div className="col">
-          <select id="statusFilter" className="form-control" onChange={(e)=>{
-            setFilter({...filter,status:e.target.value})
-          }}>
-            <option value="">All Statuses</option>
-            <option value="alive">Alive</option>
-            <option value="dead">Dead</option>
-            <option value="unknown">Unknown</option>
-          </select>
-        </div>
-        <div className="col">
-          <select id="speciesFilter" className="form-control" onChange={
-            (e)=>{
-              setFilter({...filter,species:e.target.value})
-            }
+        <div className="btn-group my-2">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">
+              <i className="fi fi-rr-settings-sliders"></i>
+        </button>
+        <div className="dropdown-menu dropdown-menu-start" aria-labelledby="triggerId">
+          <div className="col">
+            <select id="statusFilter" className="form-control" onChange={(e)=>{
+              filter={...filter,status:e.target.value};
+              filterCharacter() 
+            }}>
+              <option value="">All Statuses</option>
+              <option value="alive">Alive</option>
+              <option value="dead">Dead</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
+          <div className="col">
+            <select id="speciesFilter" className="form-control" onChange={
+              (e)=>{
+                filter = {...filter,species:e.target.value};
+                filterCharacter()
+              }
 
-          }>
-            <option value="">All Species</option>
-            <option value="Human">Human</option>
-            <option value="Alien">Alien</option>
-            
-          </select>
-        </div>
-        <div className="col">
-          <select id="genderFilter" className="form-control" onChange={
-            (e)=>{
-              setFilter({...filter,gender:e.target.value})
-            }
-          }>
-            <option value="">All Genders</option>
-            <option value="female">Female</option>
-            <option value="male">Male</option>
-            <option value="genderless">Genderless</option>
-            <option value="unknown">Unknown</option>
-          </select>
+            }>
+              <option value="">All Species</option>
+              <option value="Human">Human</option>
+              <option value="Alien">Alien</option>
+              
+            </select>
+          </div>
+          <div className="col">
+            <select id="genderFilter" className="form-control" onChange={
+              (e)=>{
+                filter = {...filter,gender:e.target.value};
+                filterCharacter()
+              }
+            }>
+              <option value="">All Genders</option>
+              <option value="female">Female</option>
+              <option value="male">Male</option>
+              <option value="genderless">Genderless</option>
+              <option value="unknown">Unknown</option>
+            </select>
+          </div>
         </div>
       </div>
-    </div>
-    
+      </div>  
     {/* characters */}
-    <div className="container-fluid">
-      <div className="row g-3">
-        {
-          (characters.length>0) && (characters.map((character:Character) => {
-            return <CharacterCard key={character.id} character={character}></CharacterCard>
-          }))
-        }
+      <div className="container-fluid ">
+        <div className="row g-3 justify-content-around pt-2">
+          {
+            !searching
+            ?(characters.length>0) 
+              ? (characters.map((character:Character) => {
+              return <CharacterCard key={character.id} character={character}></CharacterCard>
+                }))
+              :
+              <div className="d-flex justify-content-center align-items-center">
+                <h1 className="text-center text-muted">No Characters Found</h1>
+              </div>
+            :(
+              <div className="d-flex justify-content-center align-items-center">
+                <div className="spinner-border text-primary spinner-border-sm"
+                  role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            )
+          }
+        </div>
       </div>
     </div>
-
     </>
   )
 }
--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -7,19 +7,23 @@ function CharacterCard({character}:CharacterCardProps):JSX.Element{
   // console.log("character recibed",character);
   
   return (
-      <div className="card col-12 col-md-6 col-lg-4  d-flex justify-content-center" >
-            <div className="col-12 col-sm-4">
+      <div className="col-12 col-md-6 col-lg-4  d-flex justify-content-center" style={{
+        minWidth: "300px"
+      }} >
+        <div className="card">
+            <div className="col-12">
               <img src={character.image} className="card-img img-fluid" alt="Imagen"/>
             </div>
-          <div className="col-12 col-sm-8">
-            <div className="card-body h-100 d-flex flex-column justify-content-center">
-              <h5 className="card-title">{character.name}</h5>
-              <h6><strong>Status: &nbsp;</strong> <span className="card-text">{character.status}</span> </h6>
-              <h6><strong>species: &nbsp;</strong> <span className="card-text">{character.species}</span> </h6>
-              <h6><strong>gender: &nbsp;</strong> <span className="card-text">{character.gender}</span> </h6>
-              
+            <div className="col-12">
+              <div className="card-body h-100 d-flex flex-column justify-content-center">
+                <h5 className="card-title">{character.name}</h5>
+                <h6><strong>Status: &nbsp;</strong> <span className="card-text">{character.status}</span> </h6>
+                <h6><strong>species: &nbsp;</strong> <span className="card-text">{character.species}</span> </h6>
+                <h6><strong>gender: &nbsp;</strong> <span className="card-text">{character.gender}</span> </h6>
+                
+              </div>
             </div>
-          </div>
+        </div>
       </div>
   );
 };
